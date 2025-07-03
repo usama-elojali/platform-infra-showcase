c28ed7a01c28b41648cd5ea58daa25f8c0c748fb
usama-elojali <usama.elojali@sky.uk>
2025-07-03 17:01:05 +0100

    adding metrics

--- app/app.py
+++ app/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is Usama's DevOps portfolio app. and so much more! Plus his family and friends! <3"
+    return "Hello, World! This is Usama's DevOps portfolio app with metrics!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
