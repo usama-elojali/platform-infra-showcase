5bf5ee28c7d24dea732c682aafb203cc64499d67
usama-elojali <usama.elojali@sky.uk>
2025-07-03 15:38:58 +0100

    Push latest changes after cleaning repo

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This is Usama's DevOps portfolio app. and so much more!"
+    return "Hello, World! This is Usama's DevOps portfolio app. and so much more! Plus his family and friends! <3"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
